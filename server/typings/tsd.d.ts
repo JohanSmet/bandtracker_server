d9411d7764b64d720db241c08a5794b90222053b
Johan Smet <johan.smet@justcode.be>
2015-09-14 22:13:02 +0200

    include express body-parser module

--- server/typings/tsd.d.ts
+++ server/typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="mongoose/mongoose.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
