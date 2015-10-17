440194c4c5f95c1c1c6e9cf793cf270260bc3ab6
Johan Smet <johan.smet@justcode.be>
2015-10-17 15:30:06 +0200

    Add node.js package cheerio

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -8,3 +8,4 @@
 /// <reference path="jsonwebtoken/jsonwebtoken.d.ts" />
 /// <reference path="request/request.d.ts" />
 /// <reference path="form-data/form-data.d.ts" />
+/// <reference path="cheerio/cheerio.d.ts" />
