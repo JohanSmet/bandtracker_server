ceb28e5ad8946954cc55cc444cd91ea0dafa8322
Johan Smet <johan.smet@justcode.be>
2015-10-17 13:02:00 +0200

    Added Task-model

---
+++ model/ITaskPOJO.ts
@@ -0,0 +1,20 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// File 	: 	ITaskPOJO.ts
+//
+// Purpose	: 	Task Interface
+//
+// Copyright (c) 2015	Johan SMET      All rights reserved
+//
+///////////////////////////////////////////////////////////////////////////////
+
+interface ITaskPOJO {
+    taskType    : string;
+    taskParams  : string[];
+
+    dateCreated  : Date;
+    dateExecuted : Date;
+    resultOk     : Boolean;
+};
+
+export = ITaskPOJO;
---
+++ model/Task.ts
@@ -0,0 +1,35 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// File 	: 	Task.ts
+//
+// Purpose	: 	Task model
+//
+// Copyright (c) 2015	Johan SMET      All rights reserved
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import mongoose = require("mongoose");
+import ITaskPOJO = require("./ITaskPOJO");
+
+export interface ITask extends ITaskPOJO, mongoose.Document {
+}
+
+var taskSchema = new mongoose.Schema({
+    taskType    : String,
+    taskParams  : [String],
+    dateCreated : Date,
+    dateExecuted: Date,
+    resultOk    : Boolean
+});
+
+export var repository = mongoose.model<ITask>("Task", taskSchema);
+
+export function createNew(taskType: string, taskParams: string[]) : ITask {
+    var f_task = new repository();
+    f_task.taskType     = taskType;
+    f_task.taskParams   = taskParams;
+    f_task.dateCreated  = new Date();
+    f_task.dateExecuted = null;
+    f_task.resultOk     = false;
+    return f_task;
+}
